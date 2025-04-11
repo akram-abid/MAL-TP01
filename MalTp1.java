9c89154821ac1fc53ee779ddcc7b93098a91c039
akram-abid <a.abid44778@gmail.com>
2025-04-11 23:49:54 +0100

    do: create Car class with getters and setters

--- MalTp1.java
+++ MalTp1.java
@@ -1,5 +1,41 @@
-public class MalTp1{
+public class MalTp1 {
     public static void main(String args[]){
         System.out.println("Hello World!");
+    }
+}
+
+class Car {
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public void setBrand(String newBrand){
+        this.brand = newBrand;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public void setModel(String newModel){
+        this.brand = newModel;
+    }
+
+    public String getYear(){
+        return this.brand;
+    }
+
+    public void setYear(String newYear){
+        this.brand = newYear;
     }
 }
